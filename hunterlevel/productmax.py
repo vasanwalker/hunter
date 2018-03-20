2b3d69e4b08d99ddb29484ddd754b8c8bc098868
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-03-20 16:18:07 +0530

    Create productmax.py

---
+++ hunterlevel/productmax.py
@@ -0,0 +1,28 @@
+# Function to find maximum
+# product pair in arr[0..n-1]
+def maxProduct(arr, n):
+ 
+    if (n < 2):
+        print("No pairs exists")
+        return
+     
+    # Initialize max product pair
+    a = arr[0]; b = arr[1]
+ 
+    # Traverse through every possible pair
+    # and keep track of max product
+    for i in range(0, n):
+         
+        for j in range(i + 1, n):
+            if (arr[i] * arr[j] > a * b):
+                a = arr[i]; b = arr[j]
+ 
+    print("Max product pair is {", a, ",", b, "}",
+                                          sep = "")
+     
+# Driver Code
+arr = [1, 4, 3, 6, 7, 0]
+n = len(arr)
+maxProduct(arr, n)
+ 
+# This code is contributed by Smitha Dinesh Semwal.
